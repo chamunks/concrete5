ccb2309a781c8f3692ea05ac8e5c2791c6e4af26
colin <colin@web.fund>
2018-04-14 10:23:18 -0400

    debug line insert

--- start.sh
+++ start.sh
@@ -52,6 +52,11 @@ sleep 10s
 DBCHECK_RESULT=${DBCHECK}
 echo "[Info], mysqlshow indicates the presence of the database: $DBCHECK_RESULT"
 
+## Install Debug Code suggested in slack
+echo "[RUN] sed -i '103 a `if (is_numeric($fv)) { throw new \RuntimeException('Unable to import file, got code ' . $fv); }`' /var/www/html/concrete/src/Backup/ContentImporter.php"
+sed -i '103 a `if (is_numeric($fv)) { throw new \RuntimeException('Unable to import file, got code ' . $fv); }`' /var/www/html/concrete/src/Backup/ContentImporter.php
+sed '103 a `if (is_numeric($fv)) { throw new \RuntimeException('Unable to import file, got code ' . $fv); }`' /var/www/html/concrete/src/Backup/ContentImporter.php
+
 console_break
 ## To run database preseed or not to.  Then run the appliance.
 echo "[Info] Preseed Database during first run?"
@@ -68,7 +73,7 @@ if [[ "$C5_PRESEED" == yes ]]; then
     echo "[Info] No tables Found at $DB_USERNAME@$DB_SERVER in $DB_NAME using password $DB_PASSWORD"
     echo "[Info] Running C5 installation with the following settings"
     echo "[RUN]  /var/www/html/concrete/bin/concrete5 c5:install --db-server=$DB_SERVER --db-username=$DB_USERNAME --db-password=$DB_PASSWORD --db-database=$DB_NAME --site=$C5_SITE_NAME --starting-point=$C5_STARTING_POINT --admin-email=$C5_EMAIL --admin-password=$C5_PASSWORD --site-locale=$C5_LOCALE"
-    /var/www/html/concrete/bin/concrete5 c5:install -vvv \
+    /var/www/html/concrete/bin/concrete5 c5:install \
       --db-server=$DB_SERVER \
       --db-username=$DB_USERNAME \
       --db-password=$DB_PASSWORD \
