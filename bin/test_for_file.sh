08a576305f188dce8e12315fe8cb0bf4db93e384
chamunks <chamunks@gmail.com>
2018-04-15 17:57:20 -0400

    modified test_for_file.sh use echo_fancy

--- bin/test_for_file.sh
+++ bin/test_for_file.sh
@@ -3,7 +3,7 @@
 ## Maybe add a test case to ensure that the file is critical or not.
 function test_for_file() {
 	if [ ! -f '$1' ]; then
-		echo [Info] File $1 not found.
+		echof info "File $1 not found."
 	else
 		test_perm $1 $2 $3
 	fi
