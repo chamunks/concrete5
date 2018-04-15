2841a9c80f936e3d7a777099eb0e5c65893ea231
chamunks <chamunks@gmail.com>
2018-04-15 19:33:44 -0400

    No longer a function

--- bin/test_for_file.sh
+++ bin/test_for_file.sh
@@ -1,10 +1,8 @@
 ## This tests to see if a file exists and if it's permissions are correct or not.
 ## Then it attempts to assert that files permissions.
 ## Maybe add a test case to ensure that the file is critical or not.
-function test_for_file() {
-	if [ ! -f '$1' ]; then
-		echof info "File $1 not found."
-	else
-		test_perm $1 $2 $3
-	fi
-}
+if [ ! -f '$1' ]; then
+	echof info "File $1 not found."
+else
+	test_perm $1 $2 $3
+fi
