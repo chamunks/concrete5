b18b10bbe26dd99a9f79a8b59a39197bd912ef33
chamunks <chamunks@gmail.com>
2018-04-15 14:59:29 -0400

    modified echof.sh Added more cases including a catch all help.

--- bin/echof.sh
+++ bin/echof.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## Function echo_fancy echof
-## Useage: echof [run|warn|info] {Message}
+## Function echo_fancy echof all this does is makes consecutive console echo's
+## much easier to read in CI/CD environments.
+## Useage: echof [run|warn|info|fail|done|blank] {Message}
 function echof() {
 	case $1 in
 		run )
@@ -13,5 +14,20 @@ function echof() {
 		info )
 			echo "[Info] $2"
 			;;
+		fail )
+			echo "[Fail]" $2
+			;;
+		done )
+			echo "[DONE] $2"
+			;;
+		blank )
+			echo "       $2"
+			;;
+		* )
+			echof info "You've not included instructions echo_fancy understands printing usage."
+			echof info "echof [run|warn|info|fail|done|blank] {Message}"
+			echof info "Example:"
+			echof info "echof blank This is a second line echoed to console."
+			echof blank "This is a second line echoed to console."
 		esac
 }
