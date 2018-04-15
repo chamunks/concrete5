7fe526590453a2eaf50c8ef8dec30eafd23756b8
chamunks <chamunks@gmail.com>
2018-04-15 18:00:05 -0400

    modified test_for_dir.sh added comments

--- bin/test_for_dir.sh
+++ bin/test_for_dir.sh
@@ -5,17 +5,19 @@
 ## Var $2 should be your octal permissions for said directory.
 ## Var $3 should be your "user:group" names
 function test_for_dir() {
-	## Set variables from idices
-	CHK_PATH=$1
-	CHK_OWNERSHIP=$2
-	CHK_OCTAL=$3
-	if [ -d '$1' ]; then
-		test_perm $CHK_PATH $CHK_OWNERSHIP $CHK_OCTAL
-	else
-		echof info "Directory $1 was missing, creating directory and setting permissions."
-		echof run "mkdir -pv $1"
-		mkdir -pv $1
-		echof info "test_for_dir is running test_perm $CHK_PATH $CHK_OWNERSHIP $CHK_OCTAL"
-		test_perm $CHK_PATH $CHK_OWNERSHIP $CHK_OCTAL
-	fi
+  ## Set variables from idices
+  CHK_PATH=$1
+  CHK_OWNERSHIP=$2
+  CHK_OCTAL=$3
+  ## Test for directory existence if it exists then test_perm else make it so.
+  if [ -d '$1' ]; then
+    test_perm $CHK_PATH $CHK_OWNERSHIP $CHK_OCTAL
+  else
+    ## make the directory exist with the correct perms.
+    echof info "Directory $1 was missing, creating directory and setting permissions."
+    echof run "mkdir -pv $1"
+    mkdir -pv $1
+    echof info "test_for_dir is running test_perm $CHK_PATH $CHK_OWNERSHIP $CHK_OCTAL"
+    test_perm $CHK_PATH $CHK_OWNERSHIP $CHK_OCTAL
+  fi
 }
